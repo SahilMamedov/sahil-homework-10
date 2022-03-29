c8dc53ef914fd82760ec3565ba8fa44c7951fcb7
SahilMamedov <sahilim@code.edu.az>
2022-03-29 16:14:09 +0400

    Feat: Homework-10

---
+++ homework-10/Helper/Extention.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_10
+{
+   static class  Extention
+    {
+
+        public static bool ExtentionPasswordChecked( this string password)
+        {
+            
+            
+                bool islower = false;
+                bool isupper = false;
+                bool isnumber = false;
+                bool result = false;
+
+                if (password.Length >= 8)
+                {
+
+
+                    for (int i = 0; i < password.Length; i++)
+                    {
+
+
+                        if (char.IsUpper(password[i]))
+                        {
+                            isupper = true;
+
+                        }
+
+                        else if (char.IsNumber(password[i]))
+                        {
+                            isnumber = true;
+                        }
+
+                        else if (char.IsLower(password[i]))
+                        {
+                            islower = true;
+                        }
+
+
+
+                        else
+                        {
+                           
+                            return false;
+                        }
+                    }
+
+                }
+
+                else
+                {
+                   
+                    return false;
+                }
+
+                if (isupper == true && isnumber == true && islower == true)
+                {
+                    result = true;
+                    
+
+
+                }
+
+
+                return result;
+
+
+            
+
+
+
+        }
+
+
+
+    }
+}
---
+++ homework-10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace homework_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FullName daxil edin");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("emailinizi daxil edin");
+            string email = Console.ReadLine();
+            Console.WriteLine("passwordunuzu daxil en 1 boyuk herf bir balaca herif ve 1 reqem olmalidi edin");
+            string password = Console.ReadLine();
+            User user = new User(fullname, email, password);
+
+
+
+
+
+            Console.WriteLine("FullName daxil edin");
+            string Fullname = Console.ReadLine();
+            Console.WriteLine("emailinizi daxil edin");
+            string Email = Console.ReadLine();
+            Console.WriteLine("passwordunuzu daxil edin");
+            string Password = Console.ReadLine();
+
+            User user1 = new User(Fullname, Email, Password);
+
+
+        }
+
+        public interface IAccount
+        {
+            bool PassswordChecker(string password);
+
+            void ShowInfo();
+
+        }
+    
+
+
+
+
+    }
+}
